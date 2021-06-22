2d425cd0b5a68289acfab15f61e52399627bf396
Michal Moravec <michal.moravec@macadmin.cz>
2021-06-22 20:09:39 +0200

    Use shared request code in snipeit module

--- api/snipeit.py
+++ api/snipeit.py
@@ -6,6 +6,7 @@ import time
 
 import requests
 
+from .shared import APIException, contact_api
 
 class SnipeITAPIException(Exception):
     pass
@@ -47,50 +48,36 @@ headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
 logging.basicConfig(filename=log_path, level=log_level)
 
 
-def contact_api(url, payload=None, type="GET"):
-    """Send HTTP request to Snipe-IT API. Reattempt if request fails."""
-
-    for i in range(1, attempts):
-
-        try:
-            if type == "POST" or payload is not None:
-                response = requests.post(url, json=payload, headers=headers)
-            elif type == "GET":
-                response = requests.get(url, headers=headers)
-            if str(response.status_code).startswith("2"):
-                return response.json()
-
-        except (requests.ConnectionError, json.JSONDecodeError) as ex:
-            logging.error("Exception occured during API request")
-            logging.error(ex)
-
-        sleeptimer = 1 * (1.8 ** (i - 1))
-        logging.info(f"Sleeping for: {sleeptimer}s")
-        time.sleep(sleeptimer)
-
-    raise SnipeITAPIException("Unable to get valid response from API")
-
-
-def find_asset(serial):
-    """Try to find Snipe-IT asset by its serial number"""
-
-    logging.info(f"Searching for asset with serial number: {serial}")
-
-    url = f"{api_endpoint}/hardware/byserial/{serial}"
-    data = contact_api(url)
+def get_data_from_api(url):
+    try:
+        data = contact_api(url, headers)
+    except APIException as ex:
+        raise SnipeITAPIException(ex)
     rows = data.get("rows", None)
 
     if rows is None:
         raise SnipeITAPIException("Unexpected API response. Missing rows array")
     elif not rows:  # Correct response but no assent found
         return None
-    elif len(rows) > 1:
-        raise SnipeITAPIException(
-            "Unexpected API response. Multiple objects with same serial number."
-        )
 
-    # Asset found
-    return rows[0]
+    return rows
+
+def find_asset(serial):
+    """Try to find Snipe-IT asset by its serial number"""
+
+    logging.info(f"Searching for asset with serial number: {serial}")
+
+    rows = get_data_from_api(f"{api_endpoint}/hardware/byserial/{serial}")
+
+    if rows:
+        if len(rows) > 1:
+            raise SnipeITAPIException(
+                "Unexpected API response. Multiple objects with same serial number."
+            )
+        else:
+            return rows[0]
+
+    return None
 
 
 def checkout(asset_id, user_id, asset_name=None):
@@ -103,7 +90,10 @@ def checkout(asset_id, user_id, asset_name=None):
     if asset_name:
         payload["name"] = asset_name
 
-    response = contact_api(url, payload)
+    try:
+        response = contact_api(url, payload)
+    except APIException as ex:
+        raise SnipeITAPIException(ex)
 
     if response.get("status") == "error":
         messages = response.get("messages", "Unknown error")
@@ -119,7 +109,11 @@ def checkin(asset_id):
 
     url = f"{api_endpoint}/hardware/{asset_id}/checkin"
 
-    response = contact_api(url, type="POST")
+    try:
+        response = contact_api(url, type="POST")
+    except APIException as ex:
+        raise SnipeITAPIException(ex)
+
     if response.get("status") == "error":
         messages = response.get("messages", "Unknown error")
         logging.error(f"API returned error response: {messages}")
