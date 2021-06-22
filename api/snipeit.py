cace2748a62212aaddb2ea9a2a0bf9364750dacb
Michal Moravec <michal.moravec@macadmin.cz>
2021-06-22 20:54:22 +0200

    Refactor snipeit api module

--- api/snipeit.py
+++ api/snipeit.py
@@ -1,29 +1,27 @@
+"""
+Snipe-IT API module
+
+Module to communicate with defined Snipe-IT instance.
+Requires snipeit.conf JSON file within the same directory.
+"""
 import json
 import logging
 import os
 import sys
-import time
-
-import requests
 
 from .shared import APIException, contact_api
 
+logger = logging.getLogger(__name__)
+
+
 class SnipeITAPIException(Exception):
-    pass
+    """Expection for the Snipe-IT API module"""
 
 
 # Configure SnipeIT API module
 try:
     with open(os.path.join(sys.path[0], "api", "snipeit.conf")) as jfile:
         config = json.load(jfile)
-
-        log_level = config.get("log_level")
-        if not log_level:
-            log_level = "WARNING"
-
-        log_path = config.get("log_path")
-        if not log_path:
-            log_path = "/var/log/mdmsnipiter/snipeit.log"
 
         snipeit_url = config.get("url")
         if not snipeit_url:
@@ -45,10 +43,10 @@ except Exception:
 
 api_endpoint = f"{snipeit_url}/api/v1"
 headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
-logging.basicConfig(filename=log_path, level=log_level)
 
 
 def get_data_from_api(url):
+    """Return rows of date from API"""
     try:
         data = contact_api(url, headers)
     except APIException as ex:
@@ -62,17 +60,15 @@ def get_data_from_api(url):
 
     return rows
 
-def find_asset(serial):
-    """Try to find Snipe-IT asset by its serial number"""
 
-    logging.info(f"Searching for asset with serial number: {serial}")
-
-    rows = get_data_from_api(f"{api_endpoint}/hardware/byserial/{serial}")
+def get_single_item_from_api(url):
+    """Return single item via API or fail"""
+    rows = get_data_from_api(url)
 
     if rows:
         if len(rows) > 1:
             raise SnipeITAPIException(
-                "Unexpected API response. Multiple objects with same serial number."
+                "Unexpected API response. Multiple objects with the same identfier."
             )
         else:
             return rows[0]
@@ -83,7 +79,7 @@ def find_asset(serial):
 def checkout(asset_id, user_id, asset_name=None):
     """Assign user to the asset"""
 
-    logging.info(f"Checking out asset: {asset_id} to user: {user_id}")
+    logger.info(f"Checking out asset: {asset_id} to user: {user_id}")
 
     url = f"{api_endpoint}/hardware/{asset_id}/checkout"
     payload = {"checkout_to_type": "user", "assigned_user": user_id}
@@ -91,31 +87,33 @@ def checkout(asset_id, user_id, asset_name=None):
         payload["name"] = asset_name
 
     try:
-        response = contact_api(url, payload)
+        response = contact_api(url, headers, payload=payload, operation="POST")
     except APIException as ex:
         raise SnipeITAPIException(ex)
 
-    if response.get("status") == "error":
-        messages = response.get("messages", "Unknown error")
-        logging.error(f"API returner error response: {messages}")
-        return False
-    return True
+    if response.get("status") == "success":
+        return True
+
+    messages = response.get("messages", "Unknown error")
+    logger.error(f"API returner error response: {messages}")
+    return False
 
 
 def checkin(asset_id):
     """Unassign user from the asset"""
 
-    logging.info(f"Checking in asset: {asset_id}")
+    logger.info(f"Checking in asset: {asset_id}")
 
     url = f"{api_endpoint}/hardware/{asset_id}/checkin"
 
     try:
-        response = contact_api(url, type="POST")
+        response = contact_api(url, headers, operation="POST")
     except APIException as ex:
         raise SnipeITAPIException(ex)
 
-    if response.get("status") == "error":
-        messages = response.get("messages", "Unknown error")
-        logging.error(f"API returned error response: {messages}")
-        return False
-    return True
+    if response.get("status") == "success":
+        return True
+
+    messages = response.get("messages", "Unknown error")
+    logger.error(f"API returner error response: {messages}")
+    return False
